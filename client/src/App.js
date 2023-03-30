7a37199924ba1f63f0e5b1cc7327332a8900a163
cristian_a_l <lutacristian@gmail.com>
2023-03-30 14:48:07 +0200

    added bootstrap axios fontawesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       Hallo 
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+function Footer() {
+  return (
+    <div>
+        Footer v1
+        <FontAwesomeIcon icon={faEnvelope} />
+    </div>
+  )
+}
+
+export default Footer
