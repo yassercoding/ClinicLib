5aa30a74036ec22ce030f315be639d8fe6780c1e
cristian_a_l <lutacristian@gmail.com>
2023-04-10 22:26:32 +0200

    doctors card

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NewsCont1 from './NewsCont1';
 import Hero from './Hero';
+import DoctorsCards from './DoctorsCards';
 
 function Home() {
   return (
     <div>
     <Hero />
+    <DoctorsCards />
     <NewsCont1 />
     </div>
   )
