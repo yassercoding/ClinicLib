0b7cb1e088a79c042ad2142c98160d1a73685454
cristian_a_l <lutacristian@gmail.com>
2023-04-12 12:01:16 +0200

    styling log and sign

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import axios from '../axiosinstance';
 import { useNavigate } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 function Register({ setIsLoggedin }) {
   const navigate = useNavigate();
   const [user, setUser] = useState({
@@ -33,27 +37,56 @@ function Register({ setIsLoggedin }) {
       });
   };
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <label htmlFor="">Username:</label>
-      {error.username && <p className="text-danger">{error.username.message}</p>}
-      <input type="text" name="username" value={user.username} onChange={handleChange} />
-      <label htmlFor="">Email:</label>
-      {error.email && <p className="text-danger">{error.email.message}</p>}
-      <input type="email" name="email" value={user.email} onChange={handleChange} />
-      <label htmlFor="">Password:</label>
-      {error.password && <p className="text-danger">{error.password.message}</p>}
-      <input type="password" name="password" value={user.password} onChange={handleChange} />
+<Container>
 
-      <label htmlFor="">Confirm Password:</label>
-      {error.confirmPassword && <p className="text-danger">{error.confirmPassword.message}</p>}
-      <input
-        type="password"
-        name="confirmPassword"
-        value={user.confirmPassword}
-        onChange={handleChange}
-      />
-      <button>Register</button>
-    </form>
+<br/>
+        <div className="col-sm-12 green-top"><h1 className='text-center my-3'>Sign up for a ClinicLib account </h1></div>
+        <br/>
+
+      <Form onSubmit={handleSubmit}>
+
+      <Form.Group className="mb-3" controlId="">
+        <Form.Label>Username</Form.Label>
+        {error.username && <p className="text-danger">{error.username.message}</p>}
+        <Form.Control type="text" placeholder="Username" name="username" value={user.username} onChange={handleChange} />
+
+        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        {error.email && <p className="text-danger">{error.email.message}</p>}
+        <Form.Control type="email" placeholder="Enter email" name="email" value={user.email} onChange={handleChange} />
+
+        <Form.Text className="text-muted">
+          ClinicLib will never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        {error.password && <p className="text-danger">{error.password.message}</p>}
+        <Form.Control type="password" placeholder="Password" name="password" value={user.password} onChange={handleChange} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Confirm password</Form.Label>
+        {error.confirmPassword && <p className="text-danger">{error.confirmPassword.message}</p>}
+        <Form.Control type="password" placeholder="Confirm password" name="confirmPassword" value={user.confirmPassword} onChange={handleChange} />
+        
+
+      </Form.Group>
+      
+      <Button variant="success" type="submit">
+        Register
+      </Button>
+    </Form>
+
+
+    <br/>
+    <div className="col-sm-12 green-top"><h1 className='text-center my-1'>ClinicLib</h1></div>
+    <br/>
+    </Container>
   );
 }
 
