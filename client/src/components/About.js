16e713da0f8240b5fd0574987bb6dbad6a194ba7
kevin ceka <kevinkceka@gmail.com>
2023-06-14 13:42:45 +0200

    contact updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -14,7 +14,7 @@ function About() {
       <section class="introduction-section">
         <h1>About us</h1>
         <p>
-        At Clinclib, we are passionate about improving the health and well-being of people around us and around the world.
+        At Clinclib, we  are passionate about improving the health and well-being of people around us and around the world.
          As a leading medical company, we specialize in[medical devices/ pharmaceuticals/ healthcare IT/ healthcare services] and work tirelessly to develop innovative solutions that meet the needs of patients,
          healthcare providers, and payers.
 
